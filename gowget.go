01a01a7988895c5388482b2c08263ec27bbe57b2
mshr-h <mshr-h@users.noreply.github.com>
2016-01-22 13:15:38 +0900

    add gowget.go

---
+++ gowget.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+
+	"github.com/jteeuwen/go-pkg-optarg"
+)
+
+func main() {
+	optarg.Add("o", "output-document", "output filename", "")
+
+	var (
+		filename string
+	)
+
+	for opt := range optarg.Parse() {
+		switch opt.ShortName {
+		case "o":
+			filename = opt.String()
+		}
+	}
+
+	if len(optarg.Remainder) == 1 {
+		url := optarg.Remainder[0]
+
+		if len(filename) == 0 {
+			_, filename = path.Split(url)
+		}
+
+		transport := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+
+		client := &http.Client{
+			Transport: transport,
+		}
+
+		res, err := client.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		defer res.Body.Close()
+
+		file, err := os.Create(filename)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		n, err := io.Copy(file, res.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(n, "bytes downloaded.")
+	} else {
+		optarg.Usage()
+	}
+}
